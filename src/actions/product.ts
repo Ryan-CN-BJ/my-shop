f2a7e0e4a32ab059da76b27e90f7cf4817c3a45c
Ryan-CN-BJ <maorunjie2025@qq.com>
2026-03-18 21:13:50 +0800

    feat: action里面使用use cache 以及 cacheLife

--- src/actions/product.ts
+++ src/actions/product.ts
@@ -1,11 +1,13 @@
 'use server'
-
 import sql from '@/lib/db'
+import { cacheLife } from 'next/cache'
 export async function productsAction(): Promise<{
   status: number
   body: string
   data: Array<Product>
 }> {
+  'use cache'
+  cacheLife({ stale: 10 })
   const result = await sql.query('SELECT * FROM products')
   return {
     status: 200,
