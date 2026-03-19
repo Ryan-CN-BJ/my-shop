f53d2995602984360c8f65164395a6f75bad3436
Ryan-CN-BJ <maorunjie2025@qq.com>
2026-03-19 19:22:45 +0800

    feat:action 去掉cache

--- src/actions/product.ts
+++ src/actions/product.ts
@@ -1,13 +1,13 @@
 'use server'
 import sql from '@/lib/db'
-import { cacheLife } from 'next/cache'
+// import { cacheLife } from 'next/cache'
 export async function productsAction(): Promise<{
   status: number
   body: string
   data: Array<Product>
 }> {
-  'use cache'
-  cacheLife({ stale: 3000, revalidate: 60, expire: 60 * 60 * 24 })
+  // 'use cache'
+  // cacheLife({ stale: 3000, revalidate: 60, expire: 60 * 60 * 24 })
   const result = await sql.query('SELECT * FROM products')
   return {
     status: 200,
