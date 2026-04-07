bf9ece0e0cddce67883e2ce7efaf67df679d5777
Ryan-CN-BJ <maorunjie2025@qq.com>
2026-04-07 16:51:14 +0800

    feat:商品详情页添加use cache

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,8 +1,6 @@
-// 'use client'
-// import { use } from 'react'
 import { productDetailAction, productsAction } from '@/actions/product'
-// import { useEffect, useState } from 'react'
 import Image from 'next/image'
+import { Suspense } from 'react'
 import AddCart from '../components/AddCart'
 export async function generateStaticParams() {
   const { data } = await productsAction()
@@ -15,22 +13,17 @@ export async function generateStaticParams() {
     .slice(0, 5)
 }
 
-export default async function DetailPage(props: PageProps<'/detail/[id]'>) {
-  // const { id } = use(props.params)
-  // const [product, setProduct] = useState<Product>()
-  // useEffect(() => {
-  //   productDetailAction({ id: parseInt(id) }).then((product) => {
-  //     setProduct(product)
-  //   })
-  // }, [id])
-  const { id } = await props.params
+async function DetailContent({ params }: Pick<PageProps<'/detail/[id]'>, 'params'>) {
+  'use cache'
+  const { id } = await params
   const product = await productDetailAction({ id: parseInt(id) })
 
   if (!product) {
     return null
   }
+
   return (
-    <div className="w-[980] py-[25] mx-auto flex items-start">
+    <>
       <div className="w-[200]">
         <h2 className="text-[21px] mb-[10] font-bold">{product?.name}</h2>
         <p>{product?.description}</p>
@@ -49,6 +42,16 @@ export default async function DetailPage(props: PageProps<'/detail/[id]'>) {
       <div className="ml-[100]">
         <AddCart product={product} />
       </div>
+    </>
+  )
+}
+
+export default function DetailPage(props: PageProps<'/detail/[id]'>) {
+  return (
+    <div className="w-[980] py-[25] mx-auto flex items-start">
+      <Suspense fallback={<div>loading detail...</div>}>
+        <DetailContent params={props.params} />
+      </Suspense>
     </div>
   )
 }
