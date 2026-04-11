68825bca5a1795b2eaacb5e9b9d5eb738cfd2f12
Ryan-CN-BJ <maorunjie2025@qq.com>
2026-04-11 21:33:02 +0800

    feat:去掉缓存

--- src/data/product.ts
+++ src/data/product.ts
@@ -1,29 +1,29 @@
 import 'server-only'
-import { cacheLife } from 'next/cache'
+// import { cacheLife } from 'next/cache'
 import sql from '@/lib/db'
 
-const PRODUCT_CACHE_POLICY_DETAIL = {
-  stale: 60,
-  revalidate: 50,
-  expire: 60 * 60 * 24,
-} as const
+// const PRODUCT_CACHE_POLICY_DETAIL = {
+//   stale: 60,
+//   revalidate: 50,
+//   expire: 60 * 60 * 24,
+// } as const
 
-const PRODUCT_CACHE_POLICY = {
-  stale: 60,
-  revalidate: 50,
-  expire: 60 * 60 * 24,
-} as const
+// const PRODUCT_CACHE_POLICY = {
+//   stale: 60,
+//   revalidate: 50,
+//   expire: 60 * 60 * 24,
+// } as const
 
 export async function getCachedProducts(): Promise<Array<Product>> {
-  'use cache'
-  cacheLife(PRODUCT_CACHE_POLICY)
+  // 'use cache'
+  // cacheLife(PRODUCT_CACHE_POLICY)
   const result = await sql.query('SELECT * FROM products')
   return result as Array<Product>
 }
 
 export async function getCachedProductById(id: number): Promise<Product> {
-  'use cache'
-  cacheLife(PRODUCT_CACHE_POLICY_DETAIL)
+  // 'use cache'
+  // cacheLife(PRODUCT_CACHE_POLICY_DETAIL)
   const result = await sql.query('SELECT * FROM products WHERE id = $1', [id])
   return result[0] as Product
 }
